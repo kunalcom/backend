838096d291dbb4e898c22592972b6d5c2f1c71a1
Kunal Kumar <krishnakk9091@gmail.com>
2025-11-26 20:00:36 +0530

    Fix formatting of delete and getSingleJob routes

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -15,7 +15,7 @@ router.get("/getall", getAllJobs);
 router.post("/post", isAuthenticated, postJob);
 router.get("/getmyjobs", isAuthenticated, getMyJobs);
 router.put("/update/:id", isAuthenticated, updateJob);
-router.delete("/delete/:id", isAuthenticated, deleteJob);
-router.get("/:id", isAuthenticated, getSingleJob);
+router.delete("/delete/:id", isAuthenticated, deleteJob) ;
+router.get("/:id", isAuthenticated, getSingleJob);  
 
 export default router;
