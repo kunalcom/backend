0bca7fbcab3d78cbc53e3b6f57022775956f7bcf
Kunal Kumar <krishnakk9091@gmail.com>
2025-12-07 16:04:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,46 @@
+// import express from "express";
+// import dbConnection  from "./database/dbConnection.js";
+// import jobRouter from "./routes/jobRoutes.js";
+// import userRouter from "./routes/userRoutes.js";
+// import applicationRouter from "./routes/applicationRoutes.js";
+// import { config } from "dotenv";
+// import cors from "cors";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import cookieParser from "cookie-parser";
+// import fileUpload from "express-fileupload";
+
+// const app = express();
+// config({ path: "./config/config.env" });
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     method: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: "/tmp/",
+//   })
+// );
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/job", jobRouter);
+// app.use("/api/v1/application", applicationRouter);
+// dbConnection();
+
+// app.use(errorMiddleware);
+// export default app;
+
+
 import express from "express";
-import dbConnection  from "./database/dbConnection.js";
+import dbConnection from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
@@ -14,11 +55,15 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+// Allow preflight (OPTIONS)
+app.options("*", cors());
 
 app.use(cookieParser());
 app.use(express.json());
@@ -30,10 +75,13 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
 dbConnection();
 
 app.use(errorMiddleware);
+
 export default app;
